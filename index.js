ae8eb7896fcef23fc12b14c2a2225d1bdc329c74
msluther <msluther99@yahoo.com>
2019-04-01 12:18:28 -0700

    No default, no fallback, no checks, just set it

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ function CDNUp(bucket, options) {
   this.check = options.check;
   this.bucket = bucket;
   this.client = pkgcloud.storage.createClient(options.pkgcloud || {});
-  if ('acl' in options) {
-    this.acl = options.acl || 'public-read';
-  }
+  this.acl = options.acl;
   this.subdomain = options.subdomain;
 }
 
